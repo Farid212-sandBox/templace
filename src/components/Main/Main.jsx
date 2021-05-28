613bdbfdce5411264f39d8207e9018071554b043
Farid <farid.hammouten@gmail.com>
2021-05-28 18:20:12 +0900

    main ok

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Main.css'
+
+const Main = () => {
+    return (
+        <div className="Main">
+            <div className="container">
+                <div className="leftColumn">
+                    <div className="smallBox">
+                        <div className="plusIcon"></div>
+                    </div>
+                    <div className="smallBox">
+                        <div className="plusIcon"></div>
+                    </div>
+                </div>
+                <div className="rightColumn">
+                    <div className="bigBox">
+                        <div className="plusIcon"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
